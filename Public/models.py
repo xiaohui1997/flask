134055e7389cf96ead740c4b91e280debebea2ca
辉的公共仓库 <46491804+xiaohui1997@users.noreply.github.com>
2024-05-28 11:50:38 +0530

    Update models.py

--- Public/models.py
+++ Public/models.py
@@ -8,6 +8,8 @@ db = SQLAlchemy()
 '''
 def init_db(app):
     db.init_app(app)
+    with app.app_context():
+        db.create_all()
 
 class Person(db.Model):
     p_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
