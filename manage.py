51f46d07e8576202c437d32a683ab0998acc1639
fan ye <fanye>
2024-05-28 10:28:38 +0530

    重构蓝图框架

--- manage.py
+++ manage.py
@@ -6,26 +6,17 @@
 import pymysql
 from flask import Flask
 from Public import create_app
-from App.models import init_db
-from flask_script import Manager # flask-script 插件 可以接受终端参数  --help  python manage.py runservere
+from Public.models import init_db
+#from flask_script import Manager # flask-script 插件 可以接受终端参数  --help  python manage.py runservere
 #初始化蓝图
 app = create_app()
 #sqllite3 数据库连接
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite3.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite3.db'
+
 #mysql 数据库连接
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:xiaotang@192.168.144.107:3306/test1'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:xiaotang@192.168.144.107:3306/test1'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-#APP蓝图
-#from App.views import simple_page
-#app.register_blueprint(simple_page) #注册蓝图
-
-#APP1蓝图
-#from App1.views import rc
-#app.register_blueprint(rc) #注册蓝图
-
-
-manager = Manager(app=app)
 
 #db初始化
 init_db(app)
@@ -33,5 +24,5 @@ init_db(app)
 
 
 if __name__ == "__main__":
-    #app.run()
-    manager.run()
+    app.run()
+    #manager.run()
